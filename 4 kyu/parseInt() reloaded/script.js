cc869e88258a155b425aa983ab0ad79822eb0114
ubaydillah1 <ubaydillah1737@gmail.com>
2024-10-01 23:01:50 +0700

    _

---
+++ 4 kyu/parseInt() reloaded/script.js
@@ -0,0 +1,74 @@
+// In this kata we want to convert a string into an integer. The strings simply represent the numbers in words.
+
+// Examples:
+
+// "one" => 1
+// "twenty" => 20
+// "two hundred forty-six" => 246
+// "seven hundred eighty-three thousand nine hundred and nineteen" => 783919
+// Additional Notes:
+
+// The minimum number is "zero" (inclusively)
+// The maximum number, which must be supported is 1 million (inclusively)
+// The "and" in e.g. "one hundred and twenty-four" is optional, in some cases it's present and in others it's not
+// All tested numbers are valid, you don't need to validate them
+
+function parseInt(string) {
+  const wordsToNumber = {
+    zero: 0,
+    one: 1,
+    two: 2,
+    three: 3,
+    four: 4,
+    five: 5,
+    six: 6,
+    seven: 7,
+    eight: 8,
+    nine: 9,
+    ten: 10,
+    eleven: 11,
+    twelve: 12,
+    thirteen: 13,
+    fourteen: 14,
+    fifteen: 15,
+    sixteen: 16,
+    seventeen: 17,
+    eighteen: 18,
+    nineteen: 19,
+    twenty: 20,
+    thirty: 30,
+    forty: 40,
+    fifty: 50,
+    sixty: 60,
+    seventy: 70,
+    eighty: 80,
+    ninety: 90,
+    hundred: 100,
+    thousand: 1000,
+    million: 1000000,
+  };
+
+  let result = 0;
+  let current = 0;
+
+  string.split(/[\s-]+/).forEach((word) => {
+    if (word === "and") {
+      return; 
+    }
+
+    if (word in wordsToNumber) {
+      let number = wordsToNumber[word];
+      if (number === 100) {
+        current *= 100;
+      } else if (number === 1000 || number === 1000000) {
+        current *= number;
+        result += current;
+        current = 0;
+      } else {
+        current += number;
+      }
+    }
+  });
+
+  return result + current;
+}
